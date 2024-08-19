7c396709849d23726618e44e783cfbd15e2b1806
sachariase99 <sachariase99@hotmail.com>
2024-08-20 00:57:57 +0200

    Update nav.jsx
    Redid nav for the products

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <nav className="flex justify-evenly bg-[#323540] py-12 text-2xl text-white mb-4">
-        <Link className="hover:text-gray-400" to="/">Rundstykker</Link>
-        <Link className="hover:text-gray-400" to="/">Baguettes</Link>
-        <Link className="hover:text-gray-400" to="/">Franskbrød</Link>
-        <Link className="hover:text-gray-400" to="/">Kager</Link>
-        <Link className="hover:text-gray-400" to="/">Rugbrød</Link>
+        <Link className="hover:text-gray-400" to="/rundstykker">Rundstykker</Link>
+        <Link className="hover:text-gray-400" to="/baguettes">Baguettes</Link>
+        <Link className="hover:text-gray-400" to="/franskbrød">Franskbrød</Link>
+        <Link className="hover:text-gray-400" to="/kager">Kager</Link>
+        <Link className="hover:text-gray-400" to="/rugbrød">Rugbrød</Link>
     </nav>
   )
 };
